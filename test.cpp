9715ce955df69d1ff7e654fea0a1acd088b36c89
sukantoshrabon <biswassukanto203@gmail.com>
2025-08-11 22:22:48 +0600

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+using ll = long long;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
